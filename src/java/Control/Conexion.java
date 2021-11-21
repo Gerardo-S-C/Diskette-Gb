1d7463cf3b9c6a2cc8f1af81509470a26c5041d4
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2021-11-21 16:41:41 -0600

    ojalasi jale

--- src/java/Control/Conexion.java
+++ src/java/Control/Conexion.java
@@ -20,7 +20,7 @@ public class Conexion {
             Class.forName("com.mysql.jdbc.Driver");
             String url, userName, password;
             
-            url = "jdbc:mysql://us-cdbr-east-04.cleardb.com/3306/heroku_c1d2b8ca4113e8c";
+            url = "jdbc:mysql://us-cdbr-east-04.cleardb.com/heroku_c1d2b8ca4113e8c";
             userName = "b3f24c256c95db";
             password= "6dc6cf58";
             
