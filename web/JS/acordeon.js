a99a0994ebb345ccd3cf5ce0e8d3fbcb81247650
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2022-02-27 23:04:53 -0600

    padre nuestro que estas en los cielos

--- web/JS/acordeon.js
+++ web/JS/acordeon.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 const acc_btns = document.querySelectorAll(".accordion-header");
 const acc_contents = document.querySelectorAll(".accordion-body");
 
